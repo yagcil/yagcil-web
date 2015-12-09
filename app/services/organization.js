f490f3febea4b1fce47468903f63969d66d3d58b
Michal Proszek <michal.proszek@gmail.com>
2015-12-09 20:01:06 +0100

    Update organization.js

--- app/services/organization.js
+++ app/services/organization.js
@@ -4,12 +4,12 @@
 
 angular.module('ygApp')
     .factory('Organization', function ($resource) {
-        return $resource('http://localhost:5000/organization/:year/:name');
+        return $resource('http://yagcil-api.herokuapp.com/organization/:year/:name');
     })
     .factory('OrganizationRank', function ($resource) {
         // TODO(poxip): Use links from / resource
-        return $resource('http://localhost:5000/organization/:year/:name/rank');
+        return $resource('http://yagcil-api.herokuapp.com/organization/:year/:name/rank');
     })
     .factory('OrganizationStats', function ($resource) {
-        return $resource('http://localhost:5000/organization/:year/:name/stats')
+        return $resource('http://yagcil-api.herokuapp.com/organization/:year/:name/stats')
     });
